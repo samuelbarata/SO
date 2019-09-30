ae7111367c74e6487d16e19382615fc528d1befd
Samuel Barata <oliveira3@icloud.com>
2019-09-30 18:30:25 +0100

    input/output ficheiros

--- projeto/p1/main.c
+++ projeto/p1/main.c
@@ -15,16 +15,20 @@ char inputCommands[MAX_COMMANDS][MAX_INPUT_SIZE];
 int numberCommands = 0;
 int headQueue = 0;
 
+FILE *inputfile, *outputfile;
+
 static void displayUsage (const char* appName){
-    printf("Usage: %s\n", appName);
+    printf("Usage: %s inputfile outputfile\n", appName);
     exit(EXIT_FAILURE);
 }
 
 static void parseArgs (long argc, char* const argv[]){
-    if (argc != 1) {
+    if (argc != 3) {
         fprintf(stderr, "Invalid format:\n");
         displayUsage(argv[0]);
     }
+    inputfile = fopen(argv[1], "r");
+    outputfile = fopen(argv[2], "a");
 }
 
 int insertCommand(char* data) {
@@ -51,7 +55,7 @@ void errorParse(){
 void processInput(){
     char line[MAX_INPUT_SIZE];
 
-    while (fgets(line, sizeof(line)/sizeof(char), stdin)) {
+    while (fgets(line, sizeof(line)/sizeof(char), inputfile)) {
         char token;
         char name[MAX_INPUT_SIZE];
 
@@ -77,6 +81,7 @@ void processInput(){
             }
         }
     }
+    fclose(inputfile);
 }
 
 void applyCommands(){
@@ -119,13 +124,18 @@ void applyCommands(){
     }
 }
 
+//argc = numero de argumentos; argv = lista de argumentos
 int main(int argc, char* argv[]) {
     parseArgs(argc, argv);
 
     fs = new_tecnicofs();
     processInput();
+
     applyCommands();
-    print_tecnicofs_tree(stdout, fs);
+    print_tecnicofs_tree(outputfile, fs);
+
+    fflush(outputfile);
+    fclose(outputfile);
 
     free_tecnicofs(fs);
     exit(EXIT_SUCCESS);
