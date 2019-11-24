493c929fe808262555a671b482f259b6198a8c34
Samuel Barata <oliveira3@icloud.com>
2019-11-24 19:35:44 +0000

    retirar duvida e minor fixes

--- projeto/p3/lib/inodes.c
+++ projeto/p3/lib/inodes.c
@@ -190,8 +190,8 @@ int inode_set(int inumber, char *fileContents, int len){
 
 permission *permConv(char* perms){  // 1-W 2-R 3-WR
     int atoiPerms = atoi(perms);
-    int *res = malloc(sizeof(int)*2);
-    if(atoiPerms<10)    // se perm < 10 entao so tem um numero, nao devia de ser erro?
+    permission *res = malloc(sizeof(permission)*2);
+    if(atoiPerms<10)
         res[1]=NONE;
     else
         res[1] = atoiPerms%10;
