84dec479eb7e605dd4035178e8776d1e970505a2
Samuel Barata <oliveira3@icloud.com>
2019-11-27 15:02:22 +0000

    tests stuff

--- projeto/p3/api-tests/client-api-test-delete.c
+++ projeto/p3/api-tests/client-api-test-delete.c
@@ -11,6 +11,9 @@ int main(int argc, char** argv) {
         printf("Usage: %s sock_path\n", argv[0]);
         exit(0);
     }
+    char command[100]="./client-api-test-read.sh ";
+    strcat(command, argv[1]);
+
     int fd = -1;
     assert(tfsMount(argv[1]) == 0);
     assert(tfsCreate("b", RW, READ) == 0);
@@ -23,9 +26,7 @@ int main(int argc, char** argv) {
     assert(tfsUnmount() == 0);
 
     printf("SUCCESS\n\n");
-    char command[100]="./client-api-test-read.sh ";
-    strcat(command, argv[1]);
+    
     system(command);
-
     return 0;
 }
